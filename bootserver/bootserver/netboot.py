3f9d546fc33e535b9e20b6d56471b91a30067fd4
John Syrinek <john.syrinek@gmail.com>
2023-05-22 22:15:52 -0500

    finishing netboot part of bootserver

---
+++ bootserver/bootserver/netboot.py
@@ -0,0 +1,100 @@
+import os
+import shlex
+import shutil
+import subprocess
+
+from git import Repo, RemoteProgress
+from rich.progress import Progress, BarColumn, TextColumn
+from rich import print
+from rich.console import Console
+
+from .options import options
+
+
+# class CloneProgress(RemoteProgress):
+#     def __init__(self):
+#         super().__init__()
+#         self.pbar = tqdm(colour="blue")
+#
+#     def update(self, op_code, cur_count, max_count=None, message=''):
+#         self.pbar.total = max_count
+#         self.pbar.n = cur_count
+#         self.pbar.refresh()
+
+class RichProgress(RemoteProgress):
+    def __init__(self, progress: Progress):
+        self.progress = progress
+        self.title = 'Cloning'
+        self.task = progress.add_task(self.title)
+        super().__init__()
+
+    def update(self, op_code, cur_count, max_count=None, message=''):
+        operation = op_code & RemoteProgress.OP_MASK
+        stage = op_code & RemoteProgress.STAGE_MASK
+        # print(f'op_code: {op_code}, cur_count: {cur_count}, max_count: {max_count}, message: {message}')
+        # print(self._cur_line)
+        self.progress.update(self.task, total=max_count, completed=cur_count, description=message or self.title)
+        # self.progress.update(self.task, completed=float(cur_count) / max_count)
+
+
+def clone():
+    if options.overwrite_ipxe:
+        print(f"Overwriting existing iPXE at {options.ipxe}")
+        shutil.rmtree(options.ipxe, ignore_errors=True)
+
+    if os.path.exists(options.ipxe):
+        print(f"iPXE exists at {options.ipxe}")
+        return
+
+    print(f"Cloning iPXE {options.ipxe_git} ➡ {options.ipxe}")
+    with Progress(BarColumn(), TextColumn(' {task.description}'), transient=True) as p:
+        Repo.clone_from(options.ipxe_git, options.ipxe, progress=RichProgress(p))
+
+
+def render_chainloader():
+    print(f"Using {options.bootserver} in chainloader")
+    with open(options.chainload, 'r') as f:
+        template = f.read()
+    rendered = template.format(bootserver=options.bootserver)
+    with open(options.chainload_output, 'w') as f:
+        f.write(rendered)
+    print(f"Rendered chainloader script to {options.chainload_output}")
+
+
+def build():
+    command = shlex.split(
+        f"make -j24 "
+        f"{'clean ' if options.ipxe_clean else ''} "
+        f"bin-x86_64-efi/ipxe.efi "
+        f"EMBED={os.path.abspath(options.chainload_output)}"  # not an environment variable
+    )
+    # sp = yaspin(Spinners.moon, text="building iPXE")
+    # sp.start() if not options.verbose else None
+    with Progress(BarColumn(), TextColumn(' {task.description}'), transient=True) as p:
+        p.add_task('Building', start=False)
+        result = subprocess.run(
+            command,
+            cwd=f"{options.ipxe}/src",
+            text=True,
+            capture_output=not options.verbose
+        )
+    try:
+        result.check_returncode()
+    except subprocess.CalledProcessError as e:
+        print(result.stdout)
+        console = Console()
+        console.print(result.stderr, style="bold red")
+        console.print(f"Failed to compile iPXE using: {' '.join(command)}")
+
+
+def install():
+    print(f'Installing ipxe.efi to {options.static}')
+    shutil.copy(f'{options.ipxe}/src/bin-x86_64-efi/ipxe.efi', options.static)
+
+
+def prepare():
+    print("Preparing iPXE for netboot")
+    clone()
+    render_chainloader()
+    build()
+    install()
--- bootserver/bootserver/options.py
+++ bootserver/bootserver/options.py
@@ -12,9 +12,11 @@ parser.add_argument('--static', '-s', help=help("Path to static files served by 
 parser.add_argument('--ipxe', help=help("Path to iPXE git working directory"),
                     default=path.join(project_root, 'ipxe'))
 parser.add_argument('--ipxe-git', help=help("URL from which to clone iPXE"), default='https://github.com/ipxe/ipxe')
+parser.add_argument('--ipxe-clean', help=help("Clean output before building"), action='store_true')
 parser.add_argument("--overwrite-ipxe", action='store_true', help=help('Ignore existing git working directory for iPXE'))
 parser.add_argument("--bootserver", '-b', help=help('IP address for bootserver to listen on'))
 parser.add_argument("--chainload", '-c', help=help('Path to template for iPXE chainload script'), default=path.join(project_root, 'chainload.ipxe'))
 parser.add_argument("--chainload-output", help=help('Path to save rendered iPXE chainload script'), default=path.join(project_root, 'chainload.ipxe.rendered'))
+parser.add_argument("--verbose", '-v', help=help('Verbose output'), action='store_true')
 
 options = parser.parse_args()
