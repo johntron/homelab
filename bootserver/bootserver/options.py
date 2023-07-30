048e6468c376d3bdd67fb33c00494fe9dafe492a
John Syrinek <john.syrinek@gmail.com>
2023-07-30 14:40:53 -0500

    cleanup of bootserver commands

--- bootserver/bootserver/options.py
+++ bootserver/bootserver/options.py
@@ -1,7 +1,8 @@
 import argparse
-import os
 import pathlib
 from os import path
+
+project_root = path.normpath(path.join(path.dirname(__file__), '..'))
 
 
 def help(help: str):
@@ -16,27 +17,31 @@ def common_arguments(parser: argparse.ArgumentParser):
     parser.add_argument("--verbose", '-v', help=help('Verbose output'), action='store_true')
 
 
-project_root = path.normpath(path.join(path.dirname(__file__), '..'))
+def prepare_command(subparsers):
+    parser = subparsers.add_parser('prepare', description="Clone, configure, and build iPXE with chainloader")
+    parser.add_argument('--ipxe', help=help("Path to iPXE git working directory"),
+                        default=path.join(project_root, 'ipxe'))
+    parser.add_argument('--ipxe-git', help=help("URL from which to clone iPXE"),
+                        default='https://github.com/ipxe/ipxe')
+    parser.add_argument('--ipxe-clean', help=help("Clean output before building"), action='store_true')
+    parser.add_argument("--overwrite-ipxe", action='store_true',
+                        help=help('Ignore existing git working directory for iPXE'))
+    parser.add_argument("--chainload", '-c', help=help('Path to template for iPXE chainload script'),
+                        default=path.join(project_root, 'chainload.ipxe'))
+    parser.add_argument("--chainload-output", help=help('Path to save rendered iPXE chainload script'),
+                        default=path.join(project_root, 'chainload.ipxe.rendered'))
+    common_arguments(parser)
+    return parser
+
+
+def run_command(subparsers):
+    parser = subparsers.add_parser('run', description="Serves iPXE over TFTP")
+    common_arguments(parser)
+
+
 parser = argparse.ArgumentParser(prog="bootserver",
                                  description="Bootstraps a bare metal server with an operating system")
 subparsers = parser.add_subparsers(title="subcommands", dest='command', required=True)
-
-prepare_parser = subparsers.add_parser('prepare', description="Clone, configure, and build iPXE with chainloader")
-prepare_parser.add_argument('--ipxe', help=help("Path to iPXE git working directory"),
-                            default=path.join(project_root, 'ipxe'))
-prepare_parser.add_argument('--ipxe-git', help=help("URL from which to clone iPXE"),
-                            default='https://github.com/ipxe/ipxe')
-prepare_parser.add_argument('--ipxe-clean', help=help("Clean output before building"), action='store_true')
-prepare_parser.add_argument("--overwrite-ipxe", action='store_true',
-                            help=help('Ignore existing git working directory for iPXE'))
-prepare_parser.add_argument("--chainload", '-c', help=help('Path to template for iPXE chainload script'),
-                            default=path.join(project_root, 'chainload.ipxe'))
-prepare_parser.add_argument("--chainload-output", help=help('Path to save rendered iPXE chainload script'),
-                            default=path.join(project_root, 'chainload.ipxe.rendered'))
-
-run_parser = subparsers.add_parser('run', description="Serves iPXE over TFTP")
-
-common_arguments(prepare_parser)
-common_arguments(run_parser)
-
+prepare_command(subparsers)
+run_command(subparsers)
 options = parser.parse_args()
